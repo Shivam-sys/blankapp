0855e6d37c7cbe1a35019d11debdf84e82550045
Shivam Kumar <57852651+Shivam-sys@users.noreply.github.com>
2022-02-15 20:52:13 +0530

    still issue in sum

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
-
+  public n1: any = 10;
+  public n2: any = 20;
+  public sum: any = 0;
+  addition() {
+    this.sum = parseInt(this.n1) + parseInt(this.n2);
+    this.sum = parseInt(this.sum);
+  }
 }
