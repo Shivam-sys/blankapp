297ce920a789f0c201f1b769a5b5da6fe6c02194
Shivam Kumar <57852651+Shivam-sys@users.noreply.github.com>
2022-02-18 18:00:56 +0530

    first

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,12 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
-  public n1: any = 10;
-  public n2: any = 20;
+  public n1: number;
+  public n2: number;
   public sum: any = 0;
   addition() {
-    this.sum = parseInt(this.n1) + parseInt(this.n2);
-    this.sum = parseInt(this.sum);
+    this.sum = this.n1 + this.n2;
   }
+  users = [
+    {
+      Name: 'Mahir',
+      marks: '95',
+      course: 'CAP915',
+      Regno: '11980565',
+    },
+    {
+      Name: 'Shivam',
+      marks: '61',
+      course: 'CAP280',
+      Regno: '11917271',
+    },
+    {
+      Name: 'ayush',
+      marks: '36',
+      course: 'CAP392',
+      Regno: '11917896',
+    },
+    {
+      Name: 'mehak',
+      marks: '47',
+      course: 'CAP486',
+      Regno: '11912630',
+    },
+    {
+      Name: 'Joy',
+      marks: '78',
+      course: 'CAP615',
+      Regno: '11914523',
+    },
+  ];
 }
