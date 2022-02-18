0b04eab3029738fd9ba5c804dfb7a9ec7b236bd1
Shivam Kumar <57852651+Shivam-sys@users.noreply.github.com>
2022-02-18 19:12:37 +0530

    till routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'page1',
+    loadChildren: () => import('./pages/page1/page1.module').then( m => m.Page1PageModule)
+  },
+  {
+    path: 'page2',
+    loadChildren: () => import('./pages/page2/page2.module').then( m => m.Page2PageModule)
+  },
 ];
 
 @NgModule({
